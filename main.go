2a23b50d07899fd2d3d1664ee3db32c2a12c9ece
Michael C <michaelbc71@proton.me>
2024-10-22 16:50:54 -0400

    added mkdir and hostname

--- main.go
+++ main.go
@@ -9,15 +9,41 @@ import (
 )
 
 func main() {
-	fmt.Print("$ ")
-	reader := bufio.NewReader(os.Stdin)
-	line, err := reader.ReadString('\n')
-	if err != nil {
-		log.Fatal(err)
+	for {
+		fmt.Print("$ ")
+		reader := bufio.NewReader(os.Stdin)
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+		trim := strings.TrimSpace(line)// trim the line of whitespace 
+		args := strings.Split(trim," ")
+		switch args[0] {
+		case "mkdir":
+			err = os.Mkdir(args[1], 0777)
+			if err != nil {
+				log.Fatal(err)
+			}
+		case "cd" :
+			err = os.Chdir(args[1])
+			if err != nil {
+				log.Fatal(err)
+			}
+		case "pwd" :
+			err = os.Chdir(args[1])
+			if err != nil {
+				log.Fatal(err)
+			}
+		case "hostname" :
+			hostname, err := os.Hostname()
+			if err != nil {
+				log.Fatal(err)
+			}
+			fmt.Printf(hostname)
+		default:
+			
+			
+		}
 	}
-	args := strings.Split(line," ")
 
-	for i:=0; i < len(args); i++ {
-		fmt.Println(args[i])
-	}
 }
